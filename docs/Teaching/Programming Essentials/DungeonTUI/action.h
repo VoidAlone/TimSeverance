8996067709c06e0469a2b9c80c53c0e15e050633
VoidAlone <glassbox93@gmail.com>
2026-09-26 05:12:47 -0600

    idk

--- docs/Teaching/Programming Essentials/DungeonTUI/action.h
+++ docs/Teaching/Programming Essentials/DungeonTUI/action.h
@@ -1,4 +1,6 @@
 #ifndef ACTION_H
 #define ACTION_H
 
+
+
 #endif // !ACTION_H
--- docs/Teaching/Programming Essentials/DungeonTUI/main.c
+++ docs/Teaching/Programming Essentials/DungeonTUI/main.c
@@ -12,22 +12,6 @@ void quit(){
 	ui_box_t(ui_t);
 }
 
-typedef struct{
-	char* keybind;
-	func callback;
-} Keybind;
-
-void register_keybinds(){
-	FILE *cfg= fopen("./configs/keymap.cfg", "r");
-	char *action;
-	char *keybind;
-	if(cfg){
-		while(fscanf(cfg, "%32[^=]=%32s", action, keybind)){
-			Keybind keymap = {keybind, action};	
-			ui_key(keybind, action, &u);
-		}
-	}
-}
 
 int main(){
 	ui_new(0, &u);
